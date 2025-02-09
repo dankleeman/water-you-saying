eeae8d1f6ffff6679be7ead44acfeb044033402c
dankleeman <dan.kleeman.git@pm.me>
2025-02-09 12:46:31 -0600

    Adding prompts dir.

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
 import time
 
 from app import keyboard, llms
+import typer
+from pathlib import Path
 
+PROMPT_DIR_PATH = Path('prompts')
 
-def main():
+def get_prompt(prompt_file: str | None = None):
+    if prompt_file:
+        with open(PROMPT_DIR_PATH / prompt_file) as f:
+            prompt = f.read().strip()
+        return prompt
+    return None
+
+def main(user_prompt_file: str | None = None, system_prompt_file: str | None = None):
+    user_prompt = get_prompt(user_prompt_file)
+    system_prompt = get_prompt(system_prompt_file)
+
     while True:
         keyboard.wait_until_shortcut()
-        response = llms.get_open_ai_response()
+        response = llms.get_open_ai_response(user_prompt, system_prompt)
         print(response)
         keyboard.output(response)
         time.sleep(0.1)
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
