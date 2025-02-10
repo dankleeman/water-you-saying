d23ddb78fea4d3c6fbb20ef0f82bb9339cd60e0e
dankleeman <dan.kleeman.git@pm.me>
2025-02-09 18:11:34 -0600

    Adding support for screenshots for context.

---
+++ app/image.py
@@ -0,0 +1,24 @@
+import base64
+from io import BytesIO
+
+import pyautogui
+import screeninfo
+
+
+def take_screenshot(save_filename=None):
+    screens = screeninfo.get_monitors()
+    monitor = screens[1]
+    left, top, width, height = monitor.x, monitor.y, monitor.width, monitor.height
+
+    screenshot = pyautogui.screenshot(region=(left, top, width, height))
+    if save_filename:
+        screenshot.save(save_filename, format="PNG")
+
+    buffer = BytesIO()
+    screenshot.save(buffer, format="PNG")
+    return buffer
+
+def encode_image(buffer):
+    image_data = buffer.getvalue()
+    base64_string = base64.b64encode(image_data).decode("utf-8")
+    return base64_string
--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 import time
-
-from app import keyboard, llms
-import typer
 from pathlib import Path
+
+import typer
+
+from app import image, keyboard, llm
 
 PROMPT_DIR_PATH = Path('prompts')
 
@@ -13,13 +14,19 @@ def get_prompt(prompt_file: str | None = None):
         return prompt
     return None
 
-def main(user_prompt_file: str | None = None, system_prompt_file: str | None = None):
+def main(user_prompt_file: str | None = None, system_prompt_file: str | None = None, use_vision: bool = False):
     user_prompt = get_prompt(user_prompt_file)
     system_prompt = get_prompt(system_prompt_file)
 
     while True:
         keyboard.wait_until_shortcut()
-        response = llms.get_open_ai_response(user_prompt, system_prompt)
+
+        enc_image_str = None
+        if use_vision:
+            screenshot = image.take_screenshot(save_filename='image.png')
+            enc_image_str = image.encode_image(screenshot)
+
+        response = llm.get_open_ai_response(user_prompt, system_prompt, enc_image_str=enc_image_str)
         print(response)
         keyboard.output(response)
         time.sleep(0.1)
